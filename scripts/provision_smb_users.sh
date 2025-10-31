a2544ee0fd34929dfd64c8cea27db0c94f8147c7
Puluntu <puluntu@proton.me>
2025-10-31 17:52:11 +0100

    Version v0.1.1-prealpha

--- scripts/provision_smb_users.sh
+++ scripts/provision_smb_users.sh
@@ -8,20 +8,25 @@ set -euo pipefail
 ## * * * * * /srv/www/server/scripts/provision_smb_users.sh
 ## to the superuser's crontab.
 
+
 ## IMPORTANT NOTE #2!
 ## Since the queue files contain plaintext passwords (which is required temporarily to make an SMB user with smbpasswd -a),
 ## it's recommended to mount /srv/www/server/data/queue as a tmpfs filesystem in RAM.
 ## This is because RAM is volatile (and gets cleared on every reboot).
 ## If you have an HDD, there's no need to do this, since shred -u works fine for securely erasing queue files.
+## That is, UNLESS you use some specific filesystems, but more info on that in /srv/www/server/data/queue/README.txt. (PLEASE CHECK IT!!!)
 ## On SSDs, because of wear leveling, shred -u can't guarantee a full secure erase,
 ## which is why it's recommended to store the queue in RAM memory.
+
 ## WHAT SIZE SHOULD I MAKE THE TMPFS FILESYSTEM?
 ## The tmpfs filesystem doesn't need to be big. Just 10 MiB is enough for most use cases.
 ## Even 1 MiB could be enough for really small servers.
-## Check /srv/www/server/data/queue/README.txt for more info.
+## Check /srv/www/server/data/queue/README.txt for more info. THIS IS NOT OPTIONAL!!!!!!!!!!
+
 
 ## IMPORTANT NOTE #3!
 ## Replace any instance of /srv/www/server with the real path on your server!
+
 
 # All commands are invoked using variables that point to their absolute path
 # This is because $PATH is undefined when running the script with cron
@@ -34,10 +39,11 @@ SEMANAGE="/usr/sbin/semanage"
 RESTORECON="/usr/sbin/restorecon"
 SMBPASSWD="/usr/bin/smbpasswd"
 GREP="/usr/bin/grep"
-SHRED="/usr/bin/shred"
+STAT="/usr/bin/stat"
+DD="/usr/bin/dd"
+RM="/usr/bin/rm"
 
 QUEUE_DIR="/srv/www/server/data/queue"
-USERLIST="/srv/www/server/data/users.txt"
 
 echo "Starting SMB provisioning at $(date)"
 
@@ -45,7 +51,7 @@ for file in "$QUEUE_DIR"/*.rgs; do
 	[ -e "$file" ] || continue
 
 	echo "Processing $file"
-	line=$(<"$file")
+	{ read -r _; read -r line; } < $file
 	username="${line%%:*}"
 	password="${line#*:}"
 
@@ -70,7 +76,12 @@ for file in "$QUEUE_DIR"/*.rgs; do
 	} | $SMBPASSWD -a "$username" >/dev/null
 
 	# Securely erase the queue file
-	$SHRED -u "$file"
+	filesize=$($STAT --format "%s" $file)
+	$DD if=/dev/random of="$file" bs=1 count="$filesize" conv=notrunc
+	$DD if=/dev/random of="$file" bs=1 count="$filesize" conv=notrunc
+	$DD if=/dev/random of="$file" bs=1 count="$filesize" conv=notrunc
+	$DD if=/dev/zero of="$file" bs=1 count="$filesize" conv=notrunc
+	$RM "$file"
 
 	echo "Provisioned user $username"
 done
